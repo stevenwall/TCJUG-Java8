020952c6fd46b45d5d90155bfa0e62e9e96e901f
Steve Wall <stevenmwall@gmail.com>
2014-11-18 22:06:41 -0600

    Create DistanceUtils.java

---
+++ DistanceUtils.java
@@ -0,0 +1,7 @@
+package com.wall.steve.demo;
+
+public class DistanceUtils {
+	public static String getLabelStatic() {  return "Kilometers";  }
+	
+	public String getLabelInstance() {  return "KM";  }
+}
